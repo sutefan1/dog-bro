df6df2875583b7b8d882fd2c435d573b87cd99b6
Klemens Strasser <klemens.strasser@gmail.com>
2018-05-02 11:32:54 +0200

    Refined the TestScene. Added the GoalTile

---
+++ dog-bro/Assets/Scripts/Elements/GoalTileController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTileController : TactileController {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // TODO: Notify that the goal has been reached
+        Debug.Log("Wuhu - Goal Reached!");
+    }
+}
