2ce2eae674b81decee28696f9fea89550bfe5c28
aniltilanthe <aniltilanthe92@gmail.com>
2018-08-08 19:40:01 +0200

    Save last position and go to last position on death

---
+++ dog-bro/Assets/Scripts/DeathPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPoint : MonoBehaviour {
+
+    /// <summary>
+    /// Indicate if the checkpoint is activated
+    /// </summary>
+    public bool Activated = false;
+
+    private GameController gameController;
+
+    void Start()
+    {
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("DeathPoint other name " + other.name);
+        // If the player passes through the checkpoint, we activate it
+        if (other.name == "Player")
+        {
+            gameController.OnPlayerDeath();
+        }
+    }
+}
