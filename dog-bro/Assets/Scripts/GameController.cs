b4bf831b4c3accad71f0e5f62fe1834c8522a245
aniltilanthe <aniltilanthe92@gmail.com>
2018-08-08 19:52:16 +0200

    added method with name ResetPlayerToLastCheckPoint

--- dog-bro/Assets/Scripts/GameController.cs
+++ dog-bro/Assets/Scripts/GameController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
     public TextController textController;
     public BlindController blindController;
@@ -15,6 +16,8 @@ public class GameController : MonoBehaviour {
     void Start()
     {
         CameraPlayer = GameObject.Find("[CameraRig]");
+
+        //Position of Starting tile
         startPosition = Vector3.zero;
     }
     public void GoalReached()
@@ -43,15 +46,16 @@ public class GameController : MonoBehaviour {
      */
     public void UpdatePlayerLastPosition(Vector3 newPosition)
     {
-        Debug.Log("GameController UpdatePlayerLastPosition old " + CameraPlayer.transform.position);
-        Debug.Log("GameController UpdatePlayerLastPosition new " + newPosition);
-        Debug.Log("GameController UpdatePlayerLastPosition PlayerLastCheckPointPosition " + PlayerLastCheckPointPosition);
         //If a checkpoint is passed, 
         //the "startPosition" variable of "GameController" is set to this position 
         CameraPlayer.transform.position = newPosition;
         startPosition = newPosition;
-        Debug.Log("GameController UpdatePlayerLastPosition new " + CameraPlayer.transform.position);
-        Debug.Log("GameController UpdatePlayerLastPosition PlayerLastCheckPointPosition " + PlayerLastCheckPointPosition);
+    }
+
+    public void ResetPlayerToLastCheckPoint()
+    {
+        //Go To Last CheckPoint Position
+        CameraPlayer.transform.position = this.GetPlayerLastPosition();
     }
 
     public Vector3 GetPlayerLastPosition()
@@ -63,8 +67,6 @@ public class GameController : MonoBehaviour {
     {
         //On Death 
         //go to the last saved position
-        Debug.Log("On Player Death this.GetPlayerLastPosition() " + this.GetPlayerLastPosition());
-        Debug.Log("On Player Death CameraPosition " + CameraPlayer.transform.position);
         CameraPlayer.transform.position = this.GetPlayerLastPosition();
     }
 }
